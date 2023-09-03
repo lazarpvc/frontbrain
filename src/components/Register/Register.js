9fd4c8935a29a654c0c795762902e04f4d1cc107
Home User <home@h3lpavlovic.vbs>
2023-09-03 15:26:46 +0200

    changing link

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -22,7 +22,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:4000/register', {
+        fetch('https://backbrain.onrender.com/register', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     onSubmitSignIn = () => {
         const { signInEmail, signInPassword } = this.state;
 
-        fetch('http://localhost:4000/signin', {
+        fetch('https://backbrain.onrender.com/signin', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
